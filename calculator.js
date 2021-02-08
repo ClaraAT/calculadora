0357b6e7373016e0460f33104b30747544e91b44
Clara Aler <claraaler@gmail.com>
2021-02-08 23:49:39 +0100

    Script

--- calculator.js
+++ calculator.js
@@ -19,13 +19,39 @@ const display = document.querySelector('.display'); //El display
 
 //funtion dentro del Method forEach para Arays
 buttons.forEach(function(button){
-     button.addEventListener("click", calculate); //
+     button.addEventListener("click", calculate); //cuando haya click en un botón, lanzar función calculate()
 });
 
-function calculate(){}
+function calculate(event){
+     const buttonValue=event.target.value;
+     if (buttonValue === '='&& display.value!== "") { //display.value!== "" >> evitar el "Undefined" en la pantalla por un cálculo vacio
+          display.value = eval (display.value); //W3S: "The eval() function evaluates or executes an argument."
+     }
+     else{
+     display.value += buttonValue;
+     }
+}
 
-
-
+/*
+function calculate(event) {
+     // current clicked buttons value
+     const clickedButtonValue = event.target.value;
+   
+     if (clickedButtonValue === '=') {
+       // check if the display is not empty then only do the calculation
+       if (display.value !== '') {
+         // calculate and show the answer to display
+         display.value = eval(display.value);
+       }
+     } else if (clickedButtonValue === 'C') {
+       // clear everything on display
+       display.value = '';
+     } else {
+       // otherwise concatenate it to the display
+       display.value += clickedButtonValue;
+     }
+   }
+*/
 
 
 
